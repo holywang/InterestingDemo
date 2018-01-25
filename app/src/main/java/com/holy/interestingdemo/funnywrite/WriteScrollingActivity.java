9db75194bdd6b8d1fe5eb0770dad7c24bdb5bce2
holywang <664050290@qq.com>
2018-01-26 00:16:20 +0800

    漏提交的文件

--- app/src/main/java/com/holy/interestingdemo/funnywrite/WriteScrollingActivity.java
+++ app/src/main/java/com/holy/interestingdemo/funnywrite/WriteScrollingActivity.java
@@ -2,31 +2,24 @@ package com.holy.interestingdemo.funnywrite;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-
-import com.dd.CircularProgressButton;
 import com.holy.interestingdemo.R;
 
 /**
  * create by holy·wang 2018/01/18
  */
 public class WriteScrollingActivity extends AppCompatActivity {
-    CircularProgressButton writeCpb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_scrolling);
 
-        writeCpb = findViewById(R.id.write_cpb);
         initListener();
 
 
     }
 
     public void initListener(){
-        writeCpb.setOnClickListener(v -> {
 
-        });
     }
 }
